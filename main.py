586145073c522adc526ef27a6895167eb3269297
IsaacYantes23 <817296@mystma.org>
2021-02-11 17:36:09 +0000

    test 3 try 1

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-#WRITE YOUR CODE IN THIS FILE
+def countB(w):
+    letter = w[0]
+    for i in range(0, len(w)):
+        letter + w[i]
+        if w[i] == "b":
+            return w[i] + letter
+
+        else:
+            return "none"
+print(countB("baseball"))
+
+def removeLast(w):
+    letter = w
+    for i in range(0, len(w)-1):
+        letter == w
+        return letter
+print(removeLast("winter"))
+
+def sumBetweenOdd(x,y):
+    number = x % y
+    for i in range(x,y):
+        number + i
+        return number
+print(sumBetweenOdd(4,13))
+
+def firstLast(w):
+    if w[0] == w[3]:
+        return True
+    else:
+        return False
+
+print(firstLast("roar"))
+
